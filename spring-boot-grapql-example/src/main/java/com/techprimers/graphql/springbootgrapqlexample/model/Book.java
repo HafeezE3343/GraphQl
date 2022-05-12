9db95a17ed42126af7ed861a10e91a1dbe52299c
mkhan <mkhan@MKHAN-DT.ctepl.local>
2022-05-12 15:27:21 +0530

    token

---
+++ spring-boot-grapql-example/src/main/java/com/techprimers/graphql/springbootgrapqlexample/model/Book.java
@@ -0,0 +1,34 @@
+package com.techprimers.graphql.springbootgrapqlexample.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table
+@Entity
+public class Book {
+
+    public Book(String isn, String title, String publisher, String[] authors, String publishedDate) {
+    	this.isn=isn;
+    	this.title=title;
+    	this.publisher=publisher;
+    	this.authors=authors;
+    	this.publishedDate=publishedDate;
+	}
+	@Id
+    private String isn;
+    private String title;
+    private String publisher;
+    private String[] authors;
+    private String publishedDate;
+
+}
